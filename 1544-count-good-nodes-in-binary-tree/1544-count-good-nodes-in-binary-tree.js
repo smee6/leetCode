a23342b097a2cac62ca0d3f22b0f9e6c7f8cf774
서명인 (Myungin, SEO) <65226760+smee6@users.noreply.github.com>
2024-07-02 11:45:00 +0900

    Time: 122 ms (68.45%), Space: 69.5 MB (82.55%) - LeetHub

---
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function(root) {
+    function dfs(node, maxVal) {
+        if (!node) return 0;
+
+        let good = node.val >= maxVal ? 1 : 0;
+
+        maxVal = Math.max(maxVal, node.val);
+
+        good += dfs(node.left, maxVal);
+        good += dfs(node.right, maxVal);
+
+        return good;
+    }
+
+    return dfs(root, root.val);
+};

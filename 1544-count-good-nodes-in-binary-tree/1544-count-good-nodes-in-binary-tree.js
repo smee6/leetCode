5396a82719d5ed742250a5fdf473f3863a306ec2
서명인 (Myungin, SEO) <65226760+smee6@users.noreply.github.com>
2024-07-02 11:57:26 +0900

    Time: 140 ms (24.01%), Space: 71 MB (19.05%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.js
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.js
@@ -10,19 +10,19 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var goodNodes = function(root) {
-    function dfs(node, maxVal) {
-        if (!node) return 0;
-
-        let good = node.val >= maxVal ? 1 : 0;
-
-        maxVal = Math.max(maxVal, node.val);
-
-        good += dfs(node.left, maxVal);
-        good += dfs(node.right, maxVal);
-
-        return good;
-    }
-
+var goodNodes = function (root) {
     return dfs(root, root.val);
 };
+
+function dfs(node, maxVal) {
+    if (!node) return 0;
+
+    let good = node.val >= maxVal ? 1 : 0;
+
+    maxVal = Math.max(maxVal, node.val);
+
+    good += dfs(node.left, maxVal);
+    good += dfs(node.right, maxVal);
+
+    return good;
+}

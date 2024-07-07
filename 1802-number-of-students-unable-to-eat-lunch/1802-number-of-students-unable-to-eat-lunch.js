8994fb4ee59581f9ab9b6f542780fd540c649a8d
서명인 (Myungin, SEO) <65226760+smee6@users.noreply.github.com>
2024-07-07 18:03:36 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.js
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.js
@@ -3,21 +3,6 @@
  * @param {number[]} sandwiches
  * @return {number}
  */
-var countStudents = function (students, sandwiches) {
-    let retry = 0;
-    while (students.length > 0) {
-        if (students[0] === sandwiches[0]) {
-            students.shift();
-            sandwiches.shift();
-            retry = 0;
-        } else {
-            let temp = students.shift();
-            students.push(temp);
-            retry++;
-        }
-        if (retry === students.length) {
-            break;
-        }
-    }
-    return students.length
+var countStudents = function(students, sandwiches) {
+    
 };

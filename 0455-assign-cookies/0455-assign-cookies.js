b20f4becee90867046adb5aa19a2aae3ddf6c5df
서명인 (Myungin, SEO) <65226760+smee6@users.noreply.github.com>
2024-07-21 19:03:57 +0900

    Time: 79 ms (52.96%), Space: 52.5 MB (17.79%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.js
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -3,22 +3,38 @@
  * @param {number[]} s
  * @return {number}
  */
-var findContentChildren = function(g, s) {
+// var findContentChildren = function(g, s) {
+//     let content = 0;
+//     g.sort((a,b)=>b-a);
+//     s.sort((a,b)=>b-a);
+
+//     while (g.length >0) {
+//         if(s[0] >= g[0]){
+//             s.shift();
+//             g.shift();
+//             content ++;
+//         } else {
+//             g.shift();
+//         }
+//     }
+//     return content;
+// };
+
+var findContentChildren = (g,s) => {
+    // let's implement two-pointer method below
     let content = 0;
-    g.sort((a,b)=>b-a);
-    s.sort((a,b)=>b-a);
+    let ptG =0 , ptS = 0;
+    g.sort((a,b)=>a-b);
+    s.sort((a,b)=>a-b);
 
-    while (g.length >0) {
-    if(s[0]>=g[0]){
-        s.shift();
-        g.shift();
-        content ++;
-    } else {
-        g.shift();
+    while (ptG <= g.length && ptS <= s.length) {
+        if (s[ptS] >= g[ptG]) {
+            ptS ++;
+            ptG ++;
+            content ++;
+        } else {
+            ptS ++
+        }
     }
-    }
-
     return content;
-
-
-};
+}

02329f6ac545cd82598eb06f4a37f7add7b0f43d
서명인 (Myungin, SEO) <65226760+smee6@users.noreply.github.com>
2024-07-28 18:48:56 +0900

    Time: 43 ms (90.68%), Space: 48.8 MB (28.54%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.js
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.js
@@ -6,7 +6,11 @@ var tribonacci = function(n) {
     if (n === 0) return 0;
     if (n === 1 || n === 2) return 1;
 
-    let memo = [0, 1, 1];
+    let memo = {
+        "0" : 0,
+        "1" : 1,
+        "2" : 1
+    }
 
     for (let i = 3; i <= n; i++) {
         memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
